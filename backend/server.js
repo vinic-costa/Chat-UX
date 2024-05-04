eb3d8d890800a208bc86d6873906ce4159b079f8
Vini <vinicius123cs2@gmail.com>
2024-05-04 19:45:19 -0300

    ajuste pt2

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,9 @@ app.use('/api/message', messageRoutes);
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/public")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
 });
 
 app.use(notFound);
